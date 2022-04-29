c538a01322ff716b4e648a56b442995b3f959141
habib-sc <habib97.cs@gmail.com>
2022-04-30 02:31:15 +0600

    DB connection and items get endpoint done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 
 const app = express();
 
@@ -13,6 +15,29 @@ app.get('/', (req, res) => {
     res.send("Furnitory");
 });
 
+// MongoDB connection info 
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@clusterfurnitory.ckxfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try {
+        // DB Connection 
+        await client.connect();
+        const itemsCollection = client.db('Furnitory').collection('Items');
+
+        // Items get endpoint 
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const items = itemsCollection.find(query);
+            const result = await items.toArray();
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 //Server running
 app.listen(port, () => {
