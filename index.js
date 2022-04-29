bac8b634b2b2890c0a413b6720721d91a8572a8f
habib-sc <habib97.cs@gmail.com>
2022-04-30 02:12:30 +0600

    basic server config and inistalled some packages

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-{\rtf1}
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//Middlewear
+app.use(cors());
+app.use(express.json());
+
+//Root endpoint 
+app.get('/', (req, res) => {
+    res.send("Furnitory");
+});
+
+
+//Server running
+app.listen(port, () => {
+    console.log('Server is running on port', port);
+});
