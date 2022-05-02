ce4c8a7ab9dcaec969c49e9db3d66a24f74e075f
habib-sc <habib97.cs@gmail.com>
2022-05-03 02:23:34 +0600

    Added jwt in users item

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
 //Middlewear
 app.use(cors());
 app.use(express.json());
+
+const verifyJwtToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        return res.status(401).send({access: 'Unauthorized!'});
+    }
+    const token = authHeader.split(' ');
+    jwt.verify(token[1], process.env.TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({access: 'Forbidden'});
+        }else{
+            req.decoded = decoded;
+            next();
+        }
+    });
+}
 
 //Root endpoint 
 app.get('/', (req, res) => {
@@ -27,18 +44,35 @@ async function run () {
         const usersCollection= client.db('Furnitory').collection('Users');
         const blogsCollection = client.db('Furnitory').collection('Blogs');
 
-        // Item Section =========================================================
-        // Item get endpoint 
-        app.get('/items', async (req, res) => {
+        // Generationg jwt token 
+        app.post('/login', (req, res) => {
+            const email = req.body;
+            const token = jwt.sign(email, process.env.TOKEN_SECRET, {
+                expiresIn: '2d'
+            });
+            res.send({token});
+        });
+
+        // Items Section =========================================================
+        // Users Items get endpoint 
+        app.get('/users-items', verifyJwtToken, async (req, res) => {
             const email = req.query.email;
-            let query;
-            if (email) {
-                // query for getting data by email 
-                query = {addedBy: email};
-            }else {
-                // query for getting all data 
-                query = {};
+            const decodedEmail = req.decoded.email;
+            const query = {addedBy: email};
+     
+            if (email === decodedEmail) {
+                const items = itemsCollection.find(query);
+                const result = await items.toArray();
+                res.send(result);
             }
+            else{
+                res.send({access: 'Forbidden!'});
+            }
+        });
+
+        // Items get endpoint 
+        app.get('/items', async (req, res) => {
+            const query = {};
             const items = itemsCollection.find(query);
             const result = await items.toArray();
             res.send(result);
